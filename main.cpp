d91b26b0fe398a2b405da7cb15764bd195fc5a2e
Ivan Elizalde <ielizalde185@gmail.com>
2022-05-24 14:54:04 -0500

    primer commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+int main() {
+	std::cout << "Arbol - nodo" << std::endl;
+	
+	return EXIT_SUCCESS;
+}
+
+class Nodo {
+private: 
+public:
+	int* Nodeleft;
+	int* nodeRight;
+
+};
+
+
+class Arbol {
+private:
+
+public:
+	int* nodeRoot;
+
+	int agregarNodoIzquierdo(int posicion) {
+
+	}
+	int eliminarNodoDerecha(int posicion) {
+
+	}
+	int modificarNodo() {
+
+	}
+};
